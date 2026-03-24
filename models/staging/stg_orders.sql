3763746675a1173d453216baac9b154213b7c92e
Antonio-prxd <antonio.balestrapro@gmail.com>
2026-03-24 15:26:13 +0100

    Ajout de tests sur les colonnes

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -5,7 +5,11 @@ renamed as (
     select
         id as orders_id,
         customer as customer_id,
-        ordered_at as orders_date
+        ordered_at as orders_date,
+        store_id,
+        subtotal,
+        tax_paid,
+        order_total
     from source
 )
 select * from renamed
